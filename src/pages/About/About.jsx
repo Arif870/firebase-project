3b88a20ae8b8cf33b3218741ea2ad50c669c81d8
Arif870 <arifuzzaman959@gmail.com>
2024-01-14 12:39:47 +0600

    about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const About = () => {
-  return <div>This is about page</div>;
+  return (
+    <div>
+      This is about page
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        dolorem rerum eos impedit molestias quae illo aspernatur ratione quaerat
+        consectetur alias provident id non, expedita excepturi quibusdam.
+        Aliquam amet consequatur ipsam. Provident esse officiis sed porro. Ut
+        sint repudiandae eos minus esse! Iure eos odio aspernatur laboriosam ut
+        magni est, explicabo incidunt consequatur cum, saepe quod nesciunt
+        repudiandae ad similique nemo quas reiciendis. Doloremque inventore
+        fugit harum autem, impedit dolorem, blanditiis nisi, perferendis ex
+        explicabo labore culpa vitae a! Officia consectetur, numquam sequi ex
+        nam impedit. Tempore aspernatur quae fugit voluptatibus blanditiis dolor
+        minima. Voluptas molestias reprehenderit odio voluptatibus sequi.
+      </p>
+    </div>
+  );
 };
 
 export default About;
